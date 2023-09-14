95dae07a3bde6b4e40808c7eeedc9ac12a3ecf8f
shev4uk <shev4ukyuri@gmail.com>
2023-09-14 17:32:00 -0500

    add pwa

--- apps/login/src/app/app.module.ts
+++ apps/login/src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
@@ -12,6 +13,12 @@ import { appRoutes } from './app.routes';
       appRoutes,
       { initialNavigation: 'enabledBlocking' }
     ),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
