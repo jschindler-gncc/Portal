d8ab9093b6950d08d9c3084e6f1f088cc0edf80e
shev4uk <shev4ukyuri@gmail.com>
2023-09-15 14:35:43 -0500

    dynamic module federation

--- apps/dashboard/src/app/app.routes.ts
+++ apps/dashboard/src/app/app.routes.ts
@@ -1,10 +1,13 @@
+import { loadRemoteModule } from '@nx/angular/mf';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
     path: 'login',
-    loadChildren: () => import('login/Module').then((m) => m.RemoteEntryModule),
+    loadChildren: () => loadRemoteModule('login', './Module').then(
+      (m) => m.RemoteEntryModule
+    ),
   },
   {
     path: '',
