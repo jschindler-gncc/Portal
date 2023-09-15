4899b35b11d114862b5b4c33e42ca4fe6dce0b33
shev4uk <shev4ukyuri@gmail.com>
2023-09-15 18:07:36 -0500

    add top-menu library

---
+++ libs/top-menu/README.md
@@ -0,0 +1,7 @@
+# top-menu
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test top-menu` to execute the unit tests.
---
+++ libs/top-menu/src/lib/top-menu.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [TopMenuComponent],
+})
+export class TopMenuModule {}
