799ab0bad2ac9fe4bccbb9118f24859eccb17554
shev4uk <shev4ukyuri@gmail.com>
2023-09-25 16:45:12 -0500

    add dashboard and login libraries

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'dashboard',
+    loadComponent: () => import('dashboard').then((m) => m.DashboardModule),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('auth').then((m) => m.AuthModule),
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: '/login',
+  },
+];
